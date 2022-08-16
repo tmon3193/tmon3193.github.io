55269748adf655686f17704e0a5e30cfef0ae38d
Ticen Monson <ticenmonson@gmail.com>
2022-08-16 13:48:45 -0600

    Added condition of if flag is greater than 72

--- lineScript.js
+++ lineScript.js
@@ -18,34 +18,39 @@ function flagNum() {
     if (document.getElementById("T1XX").selected){
         let flag = Math.ceil(partsInTote * totalTotes / spacesOnRack) + 1 + parseInt(firstRack);
         document.getElementById("flag").innerHTML = flag;
-        
+        if (flag > 72) flag = flag - 72;
+
+        document.getElementById("flag").innerHTML = flag;
     } else if (document.getElementById("LA").selected){
         let partsInTote = amntPerTote[1];
         let flag = Math.ceil(partsInTote * totalTotes / spacesOnRack) + 1 + parseInt(firstRack);
         document.getElementById("flag").innerHTML = flag;
+        if (flag > 72) flag = flag - 72;
 
     } else if (document.getElementById("DT").selected){
         let flag = Math.ceil(partsInTote * totalTotes / spacesOnRack) + 1 + parseInt(firstRack);
         document.getElementById("flag").innerHTML = flag;
+        if (flag > 72) flag = flag - 72;
         
     } else if (document.getElementById("WS/WL").selected){
         let flag = Math.ceil(partsInTote * totalTotes / spacesOnRack) + 1 + parseInt(firstRack);
         document.getElementById("flag").innerHTML = flag;
+        if (flag > 72) flag = flag - 72;
         
     } else if (document.getElementById("Toyota").selected){
         let partsInTote = amntPerTote[2];
         let spacesOnRack = amntPerRack[1];
         let flag = Math.ceil(partsInTote * totalTotes / spacesOnRack) + 1 + parseInt(firstRack);
         document.getElementById("flag").innerHTML = flag;
+        if (flag > 72) flag = flag - 72;
         
     } else if (document.getElementById("VN").selected){
         let partsInTote = amntPerTote[3];
         let spacesOnRack = amntPerRack[2];
         let flag = Math.ceil(partsInTote * totalTotes / spacesOnRack) + 1 + parseInt(firstRack);
         document.getElementById("flag").innerHTML = flag;
+        if (flag > 72) flag = flag - 72;
     }
     
-
-
 }
 
