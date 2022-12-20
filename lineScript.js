f13437306992b23d0b2fbe72653d80236ef1b961
Ticen Monson <ticenmonson@gmail.com>
2022-12-20 01:45:28 -0700

    Added Service Builds
    Added more detailed documentation as well as functionality for VN, Nissan, and U502 service builds.

--- lineScript.js
+++ lineScript.js
@@ -1,6 +1,8 @@
-const amountsPerTote = [30, 24, 12, 5];
-const amountsPerRack = [16, 12, 8, 6];
+const amountsPerTote = [30, 24, 12, 5, 28];
+const amountsPerRack = [16, 12, 8, 6, 18];
 
+
+//Function to determine what hook number the flag will be
 function calculateFlagNumber(totalTotes, firstRack, partsInTote, spacesOnRack) {
   return Math.ceil(partsInTote * totalTotes / spacesOnRack) + 1 + (firstRack);
 }
@@ -10,7 +12,8 @@ function flagNum() {
   const firstRack = parseInt(document.querySelector("#rackStart").value);
   let partsInTote = amountsPerTote[0];
   let spacesOnRack = amountsPerRack[0];
-
+  
+  //Set value of parts in each tote and number of parts on each rack depending on programs chosen
   switch (document.querySelector("#program").value) {
     case "T1XX":
     case "LA":
@@ -31,6 +34,22 @@ function flagNum() {
     case "Rivian":
       partsInTote = amountsPerTote[1];
       spacesOnRack = amountsPerRack[2];
+      break;
+  }
+  
+  //Service Builds
+  switch (document.querySelector("#service").value) {
+    case "VN":
+      partsInTote = amountsPerTote[3];
+      spacesOnRack = amountsPerRack[3];
+      break;
+    case "Nissan":
+      partsInTote = amountsPerTote[4];
+      spacesOnRack = amountsPerRack[4];
+      break;
+    case "U502":
+      partsInTote = amountsPerTote[1];
+      spacesOnRack = amountsPerRack[0];
       break;
   }
 
